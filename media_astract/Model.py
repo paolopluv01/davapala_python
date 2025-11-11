f6c98ee3e85079af3157abcfc96b2adb4fc226fc
Paolo Pluviano <paolopluviano@icloud.com>
2025-11-11 16:15:56 +0100

    Update Model.py to a new version

--- media_astract/Model.py
+++ media_astract/Model.py
@@ -1,105 +1,83 @@
+from abc import ABC, abstractmethod
 
-# -- Modellazione classe +astratta
-class Oggetto_media:
-
+class OggettoMedia(ABC):
+    """Base abstract class for media objects."""
     def __init__(self, titolo, autore):
         self.titolo = titolo
         self.autore = autore
 
     def __repr__(self):
-        return f"{self.titolo}, {self.autore}"# -- È necessaria?
-    
+        return f"{self.titolo}, {self.autore}"
 
+class Libro(OggettoMedia):
+    num_progr = 0
+    def __init__(self, titolo, autore, editore, anno):
+        super().__init__(titolo, autore)
+        self.editore = editore
+        self.anno = anno
+        Libro.num_progr += 1
+        self.id = Libro.num_progr
 
-class Media_collection:
-    def __init__(self, nome) -> None:
+    def __repr__(self):
+        return f"{self.id}, {self.titolo}, {self.autore}, {self.editore}, {self.anno}"
+
+class Quadro(OggettoMedia):
+    num_progr = 0
+    def __init__(self, titolo, autore, tecnica, anno):
+        super().__init__(titolo, autore)
+        self.tecnica = tecnica
+        self.anno = anno
+        Quadro.num_progr += 1
+        self.id = Quadro.num_progr
+
+    def __repr__(self):
+        return f"{self.id}, {self.titolo}, {self.autore}, {self.tecnica}, {self.anno}"
+
+class Film(OggettoMedia):
+    num_progr = 0
+    def __init__(self, titolo, autore, tecnica, anno):
+        super().__init__(titolo, autore)
+        self.tecnica = tecnica
+        self.anno = anno
+        Film.num_progr += 1
+        self.id = Film.num_progr
+
+    def __repr__(self):
+        return f"{self.id}, {self.titolo}, {self.autore}, {self.tecnica}, {self.anno}"
+
+class MediaCollection:
+    def __init__(self, nome):
         self.nome = nome
         self.lista = []
 
-    def add_media(self, Oggetto_media):
-        self.lista.append(Oggetto_media)
+    def add_media(self, obj):
+        if not isinstance(obj, OggettoMedia):
+            print("Elemento non valido")
+            return
+        self.lista.append(obj)
+        print(f"{obj.__class__.__name__} inserito")
 
     def stampa_lista(self):
         for l in self.lista:
             print(l)
-        
-    
 
-# -- Modellazione classi derivate -- 
-class Libro(Oggetto_media):
-    num_progr = 0# -- variabile di classe inizializzata a 0
-    def __init__(self, titolo, autore, editore, anno):
-        super().__init__(titolo, autore)
-        self.editore = editore # -- attributi aggiunti alle classi derivate
-        self.anno = anno
-        Libro.num_progr += 1# -- variabile di classe, si aggiorna a +1 ad ogni Oggetto creato
-
-    def __repr__(self):
-        return f"{self.num_progr}, {self.titolo}, {self.autore}, {self.editore}, {self.anno}"
-
-
-class Quadro(Oggetto_media):
-    num_progr = 0
-    def __init__(self, titolo, autore, tecnica, anno):
-        super().__init__(titolo, autore)
-        self.tecnica = tecnica
-        self. anno = anno
-        Quadro.num_progr += 1
-
-    def __repr__(self):
-        return f"{self.num_progr}, {self.titolo}, {self.autore}, {self.tecnica}, {self.anno}"
-
-
-
-class Film(Oggetto_media):
-    num_progr = 0
-    def __init__(self, titolo, autore, tecnica, anno):
-        super().__init__(titolo, autore)
-        self.tecnica = tecnica
-        self. anno = anno
-        Film.num_progr += 1
-
-    def __repr__(self):
-        return f"{self.num_progr}, {self.titolo}, {self.autore}, {self.tecnica}, {self.anno}"
-
-
-class Biblioteca(Media_collection):
-    def __init__(self, nome) -> None:
-        super().__init__(nome)
-
-        
-    def add_media(self, Oggetto_media):
-        if type(Oggetto_media) == Libro:
-            super().add_media(Libro)
-            print('Libro inserito')
+class Biblioteca(MediaCollection):
+    def add_media(self, obj):
+        if isinstance(obj, Libro):
+            super().add_media(obj)
         else:
-            print('Inserimento sbagliato') 
+            print("Inserimento sbagliato: non è un Libro")
 
-
-class Pinacoteca(Media_collection):
-    def __init__(self, nome) -> None:
-        super().__init__(nome)
-
-    def add_media(self, Oggetto_media):
-        if type(Oggetto_media) == Quadro:
-            super().add_media(Oggetto_media)
-            print('Quadro inserito')
+class Pinacoteca(MediaCollection):
+    def add_media(self, obj):
+        if isinstance(obj, Quadro):
+            super().add_media(obj)
         else:
-            print('Inserimento sbagliato')
+            print("Inserimento sbagliato: non è un Quadro")
 
-
-class Videoteca(Media_collection):
-    def __init__(self, nome) -> None:
-        super().__init__(nome)
-
-    def add_media(self, Oggetto_media):
-        if type(Oggetto_media) == Film:
-            super().add_media(Oggetto_media)
-            print('Film inserito')
+class Videoteca(MediaCollection):
+    def add_media(self, obj):
+        if isinstance(obj, Film):
+            super().add_media(obj)
         else:
-            print('Inserimento sbagliato')
-
-
-
-
-
+            print("Inserimento sbagliato: non è un Film")
